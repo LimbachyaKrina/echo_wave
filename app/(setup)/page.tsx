54a136c8a40037fe32d95028afdd7f1c9508bbd8
LimbachiyaKrina <limbachiyakrina157@gmail.com>
2024-09-16 19:19:03 +0530

    Server_dialog

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,5 +1,33 @@
+import { initialProfile } from "@/lib/initial-profile"
+import { db } from "@/lib/db"
+import { redirect } from 'next/navigation'
+import { RedirectToSignIn } from '@clerk/nextjs'
+import { InitialModal } from "@/components/modals/initial-modal"
+
 const SetupPage = async () => {
-    return <div> Create a Server</div>
+    const result = await initialProfile()
+
+    if ('redirect' in result) {
+        return <RedirectToSignIn />
+    }
+
+    const { profile } = result
+
+    const server = await db.server.findFirst({
+        where: {
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (server) {
+        redirect(`/servers/${server.id}`);
+    }
+
+    return <InitialModal />
 }
 
 export default SetupPage;
--- lib/db.ts
+++ lib/db.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from "@prisma/client";
 
 declare global{
     var prisma : PrismaClient | undefined
@@ -7,5 +7,3 @@ declare global{
 export const db = globalThis.prisma || new PrismaClient()
 
 if(process.env.NODE_ENV !== 'production') globalThis.prisma = db
-
-// hot reload 
